e290818fed97b04ab55cc8289fc0463d40f92872
Indra Toyohasi <agussuharto@agus-no-imac.local>
2018-09-23 08:13:06 +0900

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Agus Suharto on 2018/09/23.
+//  Copyright © 2018年 Agus Suharto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
